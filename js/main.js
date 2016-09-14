552f9375c5f730e9f65a0a773befd4688de47869
Thaloman <kk.mackinnon@gmail.com>
2016-09-14 21:33:44 +1200

    Now displays API results for description.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 /// <reference path="C:\Users\Kyle MacKinnon\Desktop\Module 2\Steam-Web-App\typings\globals\jquery\index.d.ts" />
-// Submit image
+/*
+    Image file submission
+*/
 $('#image_selection_form').submit(function (event) {
     // Stop page refresh after form submission
     event.preventDefault();
     // Get image file
     var file = this[0].files[0];
+    // Parameters for the POST request
+    var params = {
+        "visualFeatures": "Categories, Tags, Description, Faces, ImageType, Color",
+        "details": "Celebrities"
+    };
     // Setup request
     var settings = {
-        url: "https://api.projectoxford.ai/vision/v1.0/analyze",
+        url: "https://api.projectoxford.ai/vision/v1.0/analyze?" + $.param(params),
         beforeSend: function (xhrObj) {
             xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "cc5ae4826fb44527be4e0c96cfb951b5");
@@ -28,9 +35,27 @@ $('#image_selection_form').submit(function (event) {
     })
         .fail(function (error) { alert("Request failed. Possibly too many requests? Try wait a little bit."); });
 });
+/*
+    Image file description
+*/
 function describeImage(data) {
-    alert(data);
+    $("#desc_table_body tr").empty;
+    $("#desc_sentence").append(data.description.captions[0].text);
+    $("#desc_table_body").empty();
+    var tags = "";
+    for (var _i = 0, _a = data.description.tags; _i < _a.length; _i++) {
+        var tag = _a[_i];
+        tags += tag;
+        if (tag != data.description.tags[data.description.tags.length]) {
+            tags += ", ";
+        }
+    }
+    tags = tags.substring(0, tags.length - 2);
+    $("#desc_table_body").append("<tr><td>Confidence:</td><td>" + data.description.captions[0].confidence + "</td></tr><tr><td>Tags:</td><td>" + tags + "</td></tr>");
 }
+/*
+    Image selection form update
+*/
 $('#image_selection_form').on('change', function () {
     var file = this[0].files[0];
     $('#image_selection_label')[0].innerText = file.name;
--- js/main.ts
+++ js/main.ts
@@ -1,8 +1,9 @@
 
 /// <reference path="C:\Users\Kyle MacKinnon\Desktop\Module 2\Steam-Web-App\typings\globals\jquery\index.d.ts" />
 
-// Submit image
-
+/* 
+    Image file submission 
+*/
  $('#image_selection_form').submit(
 
     function( event : Event ) : void {
@@ -11,12 +12,17 @@
         event.preventDefault();
 
         // Get image file
-        var file = (<HTMLInputElement> this[0]).files[0];
-
+        var file : File = (<HTMLInputElement> this[0]).files[0];
+        
+        // Parameters for the POST request
+        var params = {
+            "visualFeatures": "Categories, Tags, Description, Faces, ImageType, Color",
+            "details": "Celebrities",
+        };
 
         // Setup request
         var settings = {
-            url: "https://api.projectoxford.ai/vision/v1.0/analyze",
+            url: "https://api.projectoxford.ai/vision/v1.0/analyze?" + $.param(params),
             beforeSend: function (xhrObj) {
                 xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "cc5ae4826fb44527be4e0c96cfb951b5");
@@ -25,29 +31,44 @@
             data: file,
             processData: false
         }
-        
+
         // Make request
         $.ajax(settings)
 
         // Request finished successfully
-        .done(function (data) {
-            if (data.length != 0) {
-                describeImage(data);
-            } else {
-                alert("Found nothing.");
-            }
+        .done(function (data) : void {
+            if (data.length != 0)   { describeImage(data); } 
+            else                    { alert("Found nothing."); }
         })
+
         // There was an error with the request
-        .fail(function (error) { alert("Request failed. Possibly too many requests? Try wait a little bit."); });
+        .fail(function (error : Error) : void { alert("Request failed. Possibly too many requests? Try wait a little bit."); });
     }
 );
 
+/*
+    Image file description
+*/
 function describeImage(data) : void {
-    alert(data);
+    $("#desc_table_body tr").empty;
+    $("#desc_sentence").append(data.description.captions[0].text);
+    $("#desc_table_body").empty();
+    var tags : string = "";
+    for (let tag of data.description.tags) {
+        tags += tag;
+        if(tag != data.description.tags[data.description.tags.length]) {
+            tags += ", ";
+        }
+    }
+    tags = tags.substring(0,tags.length -2)
+    $("#desc_table_body").append("<tr><td>Confidence:</td><td>" + data.description.captions[0].confidence + "</td></tr><tr><td>Tags:</td><td>" + tags + "</td></tr>");
 }
 
- $('#image_selection_form').on('change', function() {
-    var file = (<HTMLInputElement> this[0]).files[0];
+/*
+    Image selection form update
+*/
+ $('#image_selection_form').on('change', function() : void {
+    var file : File = (<HTMLInputElement> this[0]).files[0];
     $('#image_selection_label')[0].innerText = file.name;
     $('#image_selection_label').show();
     $('#image_selection_submit').show();
